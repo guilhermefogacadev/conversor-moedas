39a1805835cf82213287446762f8262c2dcb3412
Guilherme Fogaça <guilhermefogacamessias@alunos.utfpr.edu.br>
2024-04-25 17:00:25 -0300

    add JOption pane, HashMaps for abbreviations and connect external API

--- src/main/java/org/example/ConversorMoedas.java
+++ src/main/java/org/example/ConversorMoedas.java
@@ -1,2 +1,59 @@
-package org.example;public class C {
+package org.example;
+
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ConversorMoedas {
+    double valor;
+    String moeda;
+
+    public ConversorMoedas(double valor, String moeda) {
+        this.valor = valor;
+        this.moeda = moeda;
+
+
+        JOptionPane.showMessageDialog(null, moeda);
+    }
+
+    public ConversorMoedas() {
+    }
+
+
+
+    public double calculoConversao(double valor, String moedaBase, String moedaAlvo) throws IOException {
+        String url_str = String.format(" https://v6.exchangerate-api.com/v6/7683def73592835d4ace6765/pair/%s/%s/%s", moedaBase, moedaAlvo, valor);
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.setRequestMethod("GET");
+        request.connect();
+        InputStream stream = request.getInputStream();
+
+        JsonParser parser = new JsonParser();
+        JsonElement root = parser.parse(new InputStreamReader(stream));
+        JsonObject jsonObj = root.getAsJsonObject();
+
+
+
+        if (jsonObj.has("conversion_result")) {
+            double conversionResult = jsonObj.get("conversion_result").getAsDouble();
+            String saida = String.format("A conversão de %s para %s com valor %.2f %s", moedaBase, moedaAlvo, conversionResult, moedaAlvo);
+            stream.close();
+            return conversionResult;
+        } else {
+            return -1;
+        }
+
+
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,52 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import javax.swing.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        double valor;
+        ConversorMoedas conversor = new ConversorMoedas();
+        String[] moedas = {"Dólar", "Peso Argentino", "Real Brasileiro", "Peso Colombiano"};
+        Map<String, String> simbolo = new HashMap<>();
+        simbolo.put("Dólar", "USD");
+        simbolo.put("Peso Argentino", "ARS");
+        simbolo.put("Real Brasileiro", "BRL");
+        simbolo.put("Peso Colombiano", "COP");
+        simbolo.put("Euro", "EUR");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        JComboBox<String> origemComboBox = new JComboBox<>(moedas);
+        JComboBox<String> destinoComboBox = new JComboBox<>(moedas);
+        JTextField valorField = new JTextField(10);
+
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Moeda de Origem:"));
+        panel.add(origemComboBox);
+        panel.add(new JLabel("Moeda de Destino:"));
+        panel.add(destinoComboBox);
+        panel.add(new JLabel("Valor:"));
+        panel.add(valorField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Conversão de Moeda", JOptionPane.OK_CANCEL_OPTION);
+        if (result == JOptionPane.OK_OPTION) {
+            String origem = (String) origemComboBox.getSelectedItem();
+            String destino = (String) destinoComboBox.getSelectedItem();
+            if(!valorField.getText().isEmpty()){
+                valor = Double.parseDouble(valorField.getText());
+                double conversao=conversor.calculoConversao(valor,simbolo.get(origem),simbolo.get(destino));
+                JOptionPane.showMessageDialog(null, "Valor convertido: " + conversao
+                        + " " + simbolo.get(destino));
+            }else{
+                JOptionPane.showMessageDialog(null, "Operação cancelada.");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Operação cancelada.");
         }
     }
+
+
 }
